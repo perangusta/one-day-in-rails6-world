5ea2a865a178003eb3d7a994010cc0e52719ef15
François Belle <francois@per-angusta.com>
2019-10-29 20:05:21 +0100

    Add Changelog title validation
    Length is 3 minimum
    Add test with sleep(3) for // testing highlight

--- app/models/changelog.rb
+++ app/models/changelog.rb
@@ -1,5 +1,5 @@
 class Changelog < ApplicationRecord
   has_rich_text :rich_description
 
-  validates :title, presence: true
+  validates :title, presence: true, length: { minimum: 3 }
 end
--- test/models/changelog_test.rb
+++ test/models/changelog_test.rb
@@ -5,5 +5,15 @@ class ChangelogTest < ActiveSupport::TestCase
     changelog = Changelog.new(title: nil)
     changelog.valid?
     assert changelog.errors.of_kind? :title, :blank
+    # highlight parallel testing
+    sleep 3
+  end
+
+  test "title is too short" do
+    changelog = Changelog.new(title: 'ab')
+    changelog.valid?
+    assert changelog.errors.of_kind? :title, :too_short
+    # highlight parallel testing
+    sleep 3
   end
 end
